cda563d5df0145da39290d16b2774e9c07bc9809
nguyenminhquang <nnguyenminhquang786@gmail.com>
2025-03-04 22:36:32 +0700

    delete all

--- DatabaseWebRESTAPI/Domain/Api/LoginRequestModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DatabaseWebRESTAPI.Domain.Api
-{
-    public class LoginRequestModel
-    {
-        public string? Username { get; set; }
-        public string? Password { get; set; }
-    }
-}
--- MAUIProject/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MAUIProject;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
